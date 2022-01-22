905bba04d27d56c9cfd470088bf4c178efacfdb0
Lennon Mudenda <lensig13@gmail.com>
2022-01-23 00:13:51 +0200

    ch-breakup-base-layout-into-blades

---
+++ resources/views/templates/folio/layouts/base.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Folio Bootstrap Template - Index</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="folio/img/favicon.png" rel="icon">
+    <link href="folio/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,500,500i,600,600i,700,700i|Playfair+Display:400,400i,500,500i,600,600i,700,700i,900,900i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="folio/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="folio/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="folio/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="folio/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="folio/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="folio/css/style.css" rel="stylesheet">
+
+    <!-- =======================================================
+  * Template Name: Folio - v4.7.0
+  * Template URL: https://bootstrapmade.com/folio-bootstrap-portfolio-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+    <!-- ======= Header Section ======= -->
+    @include('templates.folio.partials.header')
+
+    <!-- ======= Main Section ======= -->
+    @yield('main')
+
+    <!-- ======= Footer ======= -->
+    @include('templates.folio.partials.footer')
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="folio/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="folio/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="folio/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="folio/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="folio/vendor/typed.js/typed.min.js"></script>
+    <script src="folio/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="folio/js/main.js"></script>
+
+</body>
+
+</html>
